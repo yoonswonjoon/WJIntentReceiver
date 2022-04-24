4922523188b4a44bc62089e0c9849fcf1dda3c40
dooju <doojoons@naver.com>
2022-04-24 12:49:04 +0900

    a

--- app/src/main/java/com/vlm/wjintentreceiver/BActivity.kt
+++ app/src/main/java/com/vlm/wjintentreceiver/BActivity.kt
@@ -1,9 +1,12 @@
 package com.vlm.wjintentreceiver
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
@@ -23,6 +26,25 @@ class BActivity : ComponentActivity() {
                 ) {
                     if(intent != null){
                         Greeting(intent?.extras?.getString("MY_NAME") ?: "이름 모름")
+                        val int = intent?.extras?.get("Intent")
+                        int?.let{
+                            val pi = try {
+                                it as PendingIntent
+                            }catch (e:Exception){
+                                it as Intent
+                            }
+
+                            Button(onClick = {
+                                try {
+                                    (pi as PendingIntent).send()
+                                }catch (e:java.lang.Exception){
+                                    startActivity(pi as Intent)
+                                }
+                            }) {
+
+                            }
+                        }
+
                     }else{
                         Greeting("내가 그 기능 할게")
                     }
